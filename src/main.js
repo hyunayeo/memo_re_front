760095d212e42d75bd43aca18db3b525a5ffcb71
Lyrics109 <rily1108@gmail.com>
2023-11-29 16:59:25 +0900

    login 1차 완성

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 
 import "bootstrap";
+// import "bootstrap/dist/js/bootstrap.bundle.min"
 import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const app = createApp(App);
 app.use(router).mount("#app");
