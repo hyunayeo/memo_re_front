968b53891378a30717188ca483841c117408da28
devnabi <klkl0812@naver.com>
2023-12-01 16:19:46 +0900

    Resolve Conflicts

--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -2,78 +2,41 @@
   <div class="login">
     <section class="vh-90" style="background-color: #ffffff">
       <div class="container py-5 h-100">
-        <div
-          class="d-flex flex-row justify-content-center align-items-center h-100"
-        >
+        <div class="d-flex flex-row justify-content-center align-items-center h-100">
           <div class="col-md-8 col-lg-5">
             <div class="card" style="border-radius: 1rem">
               <div class="row g-0">
                 <div class="card-body text-black">
                   <form>
                     <div class="d-flex align-items-center mb-3 pb-1">
-                      <i
-                        class="fas fa-cubes fa-2x me-3"
-                        style="color: #ff6219"
-                      ></i>
+                      <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219"></i>
                       <span class="h1 fw-bold mb-0">회원가입</span>
                     </div>
 
                     <p>&nbsp;&nbsp;</p>
 
                     <div class="form-outline mb-4">
-                      <label class="form-label" for="form2Example17"
-                        >이메일 주소</label
-                      >
-                      <input
-                        type="email"
-                        id="email"
-                        class="form-control form-control-lg"
-                      />
-                      <button
-                        type="button"
-                        class="btn btn-dark btn-lg float-end"
-                        id="emailCheck"
-                      >
+                      <label class="form-label" for="form2Example17">이메일 주소</label>
+                      <input type="email" id="email" class="form-control form-control-lg" />
+                      <button type="button" class="btn btn-dark btn-lg float-end" id="emailCheck">
                         중복확인
                       </button>
                     </div>
 
                     <div class="form-outline mb-4">
-                      <label class="form-label" for="form2Example27"
-                        >비밀번호</label
-                      >
-                      <input
-                        type="password"
-                        id="password1"
-                        class="form-control form-control-lg"
-                      />
+                      <label class="form-label" for="form2Example27">비밀번호</label>
+                      <input type="password" id="password1" class="form-control form-control-lg" />
                     </div>
 
                     <div class="form-outline mb-4">
-                      <label class="form-label" for="form2Example27"
-                        >비밀번호 확인</label
-                      >
-                      <input
-                        type="password"
-                        id="password2"
-                        class="form-control form-control-lg"
-                      />
+                      <label class="form-label" for="form2Example27">비밀번호 확인</label>
+                      <input type="password" id="password2" class="form-control form-control-lg" />
                     </div>
 
                     <div class="form-outline mb-4">
-                      <label class="form-label" for="form2Example27"
-                        >닉네임</label
-                      >
-                      <input
-                        type="text"
-                        id="name"
-                        class="form-control form-control-lg"
-                      />
-                      <button
-                        type="button"
-                        class="btn btn-dark btn-lg float-end d-inline"
-                        id="nameCheck"
-                      >
+                      <label class="form-label" for="form2Example27">닉네임</label>
+                      <input type="text" id="name" class="form-control form-control-lg" />
+                      <button type="button" class="btn btn-dark btn-lg float-end d-inline" id="nameCheck">
                         중복확인
                       </button>
                     </div>
@@ -86,9 +49,7 @@
 
                     <p class="mb-5 pb-lg-2" style="text-align: center">
                       이미 Memo.re 회원인가요?
-                      <a href="/login" style="color: #6b21a8"
-                        >로그인하러 가기</a
-                      >
+                      <a href="/login" style="color: #6b21a8">로그인하러 가기</a>
                     </p>
                   </form>
                 </div>
@@ -105,13 +66,8 @@
 export default {
   data() {
     return {
-<<<<<<< HEAD
-      
-    }
-=======
       style: "color: magenta;",
     };
->>>>>>> 6bbbe8ad030bcd74cb94b90beac13d3df90380de
   },
 
   methods: {},
@@ -120,44 +76,4 @@ export default {
 };
 </script>
 
-<<<<<<< HEAD
-<style>
-.signup {
-  text-align: center;
-  margin-bottom: 15px;
-}
-
-.signup h1 {
-  color: black;
-}
-
-.form {
-  background: white;
-  border-radius: 1rem;
-  border-color: blueviolet;
-  border-style: solid;
-  border-width: 1px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.form-row {
-  margin-top: 50px;
-  margin-bottom: 50px;
-}
-
-.form-control {
-  width: 300px;
-  height: 50px;
-  margin-bottom: 10px;
-  font-size: large;
-}
-
-.large-label {
-  font-size: 22px;
-}
-</style>
-=======
 <style></style>
->>>>>>> 6bbbe8ad030bcd74cb94b90beac13d3df90380de
