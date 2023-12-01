0b4cacd6f41ccd46ee267faaed982430d5fbc4e6
kitri.ha <picnic935@gmail.com>
2023-12-01 14:52:44 +0900

    mypage 구성, 카드 이미지 수정

--- src/components/CardSmall.vue
+++ src/components/CardSmall.vue
@@ -3,17 +3,12 @@
     class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
     href="#"
   >
-    <svg
+    <img
+      src="@/assets/book_sample.jpg"
       class="bd-placeholder-img"
-      width="100%"
-      height="96"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      preserveAspectRatio="xMidYMid slice"
-      focusable="false"
-    >
-      <rect width="100%" height="100%" fill="#777" />
-    </svg>
+      height="90"
+      onerror="@/assets/profile_sample.jpg"
+    />
     <div class="col-lg-8">
       <h6 class="mb-0">Example blog post title</h6>
       <small class="text-body-secondary">January 15, 2023</small>
--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,9 +1,43 @@
 <template>
-  <h1>마이페이지 입니다</h1>
+  <div class="row g-5">
+    <div class="col-md-3">
+      <MyNavVue />
+    </div>
+
+    <div class="col-md-9">
+      <div class="position-sticky" style="top: 2rem">
+        <div class="p-4 mb-3 bg-body-tertiary rounded">
+          <h4 class="fst-italic">About</h4>
+          <p class="mb-0">
+            Customize this section to tell your visitors a little bit about your
+            publication, writers, content, or something else entirely. Totally
+            up to you.
+          </p>
+        </div>
+
+        <div>
+          <h4 class="fst-italic">Recent posts</h4>
+          <ul class="list-unstyled">
+            <li>
+              <card-small-vue />
+            </li>
+            <li>
+              <card-small-vue />
+            </li>
+            <li>
+              <card-small-vue />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import CardSmallVue from "@/components/CardSmall.vue";
+import MyNavVue from "@/components/MyNav.vue";
+export default { components: { CardSmallVue, MyNavVue } };
 </script>
 
 <style></style>
