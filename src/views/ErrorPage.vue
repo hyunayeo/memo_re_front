f82746b34dd0046218bf256dc8f9eece3edf09e0
devnabi <klkl0812@naver.com>
2023-12-01 13:20:22 +0900

    Update Vue

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -1,22 +1,13 @@
 <template>
-    <div class="error-container">
-
-        <div class="alert alert-dismissible alert-danger" style="display: grid; place-items: center;">
-            <div class="error">
-                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                <h1><strong>Error</strong></h1>
-                <h2>새로운 페이지를 발견하셨군요!</h2>
-            </div>
+    <div class="d-flex align-items-center justify-content-center vh-100 mb-5 pb-5">
+        <div class="text-center mb-5 pb-5">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-3"> <span class="text-danger">ERROR!</span> 새로운 페이지를 발견하셨군요!</p>
+            <p class="lead">
+                페이지가 존재하지 않습니다.
+            </p>
+            <a href="index.html" class="btn btn-primary btn-lg px-4 py-2">Go Home</a>
         </div>
-
-        <div class="search">
-            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-search"
-                viewBox="0 0 16 16">
-                <path
-                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-            </svg>
-        </div>
-
     </div>
 </template>
 
@@ -26,21 +17,9 @@ export default {
         return {
 
         }
-    },
-
-    methods: {
-    },
-
-    components: {
     }
 }
 </script>
 
 <style>
-.search {
-    display: grid;
-    place-items: center;
-    margin-top: 150px;
-    margin-bottom: 150px;
-}
 </style>
