ae816c9febc59fbd8a5ee09d20fdab39230538a7
captain-yun <jslovejs182@gmail.com>
2023-12-01 17:58:27 +0900

    메인화면 내 최신 게시글 동적 데이터로 보여주기 - MainPage.vue 에서 CardMedium.vue으로 props 전달 - CardMedium.vue 에서 동적 데이터 보여주기

--- src/components/CardMedium.vue
+++ src/components/CardMedium.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-md-6">
+  <div class="col-md-6" v-if="article">
     <div
       class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"
     >
@@ -10,15 +10,21 @@
           width="200"
         />
         <title>Placeholder</title>
+    
         <rect width="100%" height="100%" fill="#55595c" />
       </div>
       <div class="col p-4 d-flex flex-column position-static">
         <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
-        <h3 class="mb-0">Featured post</h3>
-        <div class="mb-1 text-body-secondary">Nov 12</div>
-        <p class="card-text mb-auto">
+        <!-- <h3 class="mb-0">Featured post</h3> -->
+        <h3 class="mb-0">{{article.title}}</h3>
+        <!-- <div class="mb-1 text-body-secondary">Nov 12</div> -->
+        <div class="mb-1 text-body-secondary">{{article.createdAt}}</div>
+        <!-- <p class="card-text mb-auto">
           This is a wider card with supporting text below as a natural lead-in
           to additional content.
+        </p> -->
+        <p class="card-text mb-auto">
+          {{article.content}}
         </p>
         <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
           Continue reading
@@ -30,7 +36,12 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props : {
+    article : Object,
+    book_img : String,
+  }
+};
 </script>
 
 <style></style>
