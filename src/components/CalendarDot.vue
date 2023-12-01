79fafd4b8fa4d060872e1aac246377f8c9536d94
Lyrics109 <rily1108@gmail.com>
2023-12-01 16:34:31 +0900

    review reflect

---
+++ src/components/CalendarDot.vue
@@ -0,0 +1,66 @@
+<template>
+    <VCalendar expanded :rows="2" :attributes="attributes" />
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const todos = ref([
+  {
+    description: "Take Noah to basketball practice.",
+    isComplete: false,
+    dates: [
+      new Date(2018, 0, 1), // Jan 1st
+      new Date(2018, 0, 10),
+    ],
+    color: "blue",
+  },
+  {
+    description: "Test code 입니다.",
+    isComplete: false,
+    dates: [
+      new Date(2018, 0, 1), // Jan 1st
+      new Date(2018, 0, 1),
+      new Date(2018, 0, 1),
+      new Date(2018, 0, 10),
+      {
+        start: new Date(2018, 0, 1), // Jan 1st
+        end: new Date(2018, 0, 4),
+      },
+      {
+        start: new Date(2018, 0, 1), // Jan 1st
+        end: new Date(2018, 0, 4),
+      },
+      {
+        start: new Date(2018, 0, 1), // Jan 1st
+        end: new Date(2018, 0, 4),
+      },
+      {
+        start: new Date(2018, 0, 1), // Jan 1st
+        end: new Date(2018, 0, 4),
+      },
+      {
+        start: new Date(2018, 0, 1), // Jan 1st
+        end: new Date(2018, 0, 4),
+      }, // - Jan 4th
+    ],
+    color: "red",
+  },
+]);
+
+const attributes = computed(() => [
+  // Attributes for todos
+  ...todos.value.map((todo) => ({
+    dates: todo.dates,
+    dot: {
+      color: todo.color,
+      ...(todo.isComplete && { class: "opacity-75" }),
+    },
+    popover: {
+      label: todo.description,
+    },
+  })),
+]);
+</script>
+
+<style></style>
--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,9 +1,16 @@
 <template>
   <h1>마이페이지 입니다</h1>
+  <calendar-dot-vue />
+
 </template>
 
 <script>
-export default {};
+import CalendarDotVue from "@/components/CalendarDot.vue";
+export default {
+  components: {
+    CalendarDotVue,
+  },
+};
 </script>
 
 <style></style>
