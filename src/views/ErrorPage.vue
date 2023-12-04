67fb166c1aa1c12183625837b1d134495a978608
devnabi <klkl0812@naver.com>
2023-12-04 09:15:15 +0900

    vue파일 추가 (#17)
    * 이메일인증 모달 추가
    
    * 에러페이지 추가

---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="d-flex align-items-center justify-content-center vh-100 mb-5 pb-5">
+        <div class="text-center mb-5 pb-5">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-3"> <span class="text-danger">ERROR!</span> 새로운 페이지를 발견하셨군요!</p>
+            <p class="lead">
+                페이지가 존재하지 않습니다.
+            </p>
+            <a href="index.html" class="btn btn-primary btn-lg px-4 py-2">Go Home</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+</style>
