3e624d411830bed84af69ba5e9176fe18d88bd77
hansolkim <67495641+Lyrics109@users.noreply.github.com>
2023-12-06 16:18:12 +0900

    ask detail 추가 (#43)

---
+++ src/views/AskDetail.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="ask-detail">
+    <form>
+      <table class="ask-contents">
+        <colgroup>
+          <col width="15%" />
+          <col width="*" />
+        </colgroup>
+        <tr>
+          <th>제목</th>
+          <td>{{ title }}</td>
+        </tr>
+        <tr>
+          <th>작성일</th>
+          <td>{{ created_at }}</td>
+        </tr>
+        <tr>
+          <th>내용</th>
+          <td>{{ contents }}</td>
+        </tr>
+      </table>
+    </form>
+    <p></p>
+    <div class="row">
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button
+          type="button"
+          v-on:click="fnUpdate"
+          class="btn btn-primary btn-lg"
+        >
+          수정
+        </button>
+        <button
+          @click="confirmDeletion"
+          type="button"
+          v-on:click="fnDelete"
+          class="btn btn-outline-primary btn-lg"
+        >
+          삭제
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "문의드립니다",
+      contents:
+        "타이머 만들어주세요. 진짜 급해요!! 저 빨리 타이머 쓰고 싶다구요!! 빨리 만드세요 개발자!! 일하세요!",
+      created_at: "2023.11.12",
+    };
+  },
+  methods: {
+    confirmDeletion() {
+      confirm("정말 삭제하시겠습니까?");
+    },
+  },
+};
+</script>
--- src/views/AskInsert.vue
+++ src/views/AskInsert.vue
@@ -4,7 +4,7 @@
       <h1>문의하기</h1>
 
       <div class="col-9 form-group">
-        <label class="col-form-label col-form-label-lg mt-4" for="inquiryTitle"
+        <label class="col-form-label col-form-label-lg mt-4" for="askTitle"
           >문의 제목</label
         >
         <input
@@ -12,22 +12,19 @@
           type="text"
           maxlength="40"
           placeholder="글자수 제한 40자"
-          id="inquiryTitle"
+          id="askTitle"
         />
       </div>
 
       <div class="col-9 form-group">
-        <label
-          class="col-form-label col-form-label-lg mt-4"
-          for="inquiryContent"
+        <label class="col-form-label col-form-label-lg mt-4" for="askContent"
           >문의 내용</label
         >
-        <textarea class="form-control" id="inquiryContent" rows="20">
-        </textarea>
+        <textarea class="form-control" id="askContent" rows="20"> </textarea>
       </div>
     </div>
     <p></p>
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+    <div class="d-grid gap-2 d-md-flex col- justify-content-md-end">
       <button type="button" class="btn btn-primary btn-lg mx-2">저장</button>
     </div>
   </div>
