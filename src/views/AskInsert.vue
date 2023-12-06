a6f835b30def5eb6323d2900f2da50aa9f0354fd
hansolkim <67495641+Lyrics109@users.noreply.github.com>
2023-12-06 14:47:54 +0900

    Inquiry 에서 ask 이름 변경 및 전반적인 수정 (#40)
    * login email form detail 변경
    
    * inquiry -> ask 이름 및 전반 수정

---
+++ src/views/AskInsert.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <h1>문의하기</h1>
+
+      <div class="col-9 form-group">
+        <label class="col-form-label col-form-label-lg mt-4" for="inquiryTitle"
+          >문의 제목</label
+        >
+        <input
+          class="form-control form-control-lg"
+          type="text"
+          maxlength="40"
+          placeholder="글자수 제한 40자"
+          id="inquiryTitle"
+        />
+      </div>
+
+      <div class="col-9 form-group">
+        <label
+          class="col-form-label col-form-label-lg mt-4"
+          for="inquiryContent"
+          >문의 내용</label
+        >
+        <textarea class="form-control" id="inquiryContent" rows="20">
+        </textarea>
+      </div>
+    </div>
+    <p></p>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <button type="button" class="btn btn-primary btn-lg mx-2">저장</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
