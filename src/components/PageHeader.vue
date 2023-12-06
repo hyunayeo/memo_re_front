e69d58b9c16bedcb055ba5dbb876efb844471db6
kitri.ha <picnic935@gmail.com>
2023-12-06 09:42:10 +0900

    메인 검색창 디자인변경

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -12,47 +12,45 @@
         >
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
-        <div class="btn-group">
-          <button
-            type="button"
-            class="btn border-secondary dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            도서
-          </button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">제목</a></li>
-            <li><a class="dropdown-item" href="#">저자</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#"></a></li>
-            <li><a class="dropdown-item" href="#">작성자</a></li>
-          </ul>
-        </div>
-        <div class="input-group">
-          <input class="" type="text" />
-          <a class="link-secondary" href="#" aria-label="Search">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="mx-3"
-              role="img"
-              viewBox="0 0 24 24"
+        <form class="form-inline">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <button
+                type="button"
+                class="btn border-secondary dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                도서
+              </button>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">도서명</a></li>
+                <li><a class="dropdown-item" href="#">작가명</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li><a class="dropdown-item" href="#">제목</a></li>
+                <li><a class="dropdown-item" href="#">작성자명</a></li>
+              </ul>
+            </div>
+            <input
+              type="text"
+              class="form-control"
+              aria-label="Text input with dropdown button"
+            />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              V
+            </button>
+          </div>
+        </form>
+        <div>
+          <div>
+            <a class="btn btn-sm btn-outline-secondary mx-1" href="/login"
+              >Login</a
             >
-              <title>Search</title>
-              <circle cx="10.5" cy="10.5" r="7.5" />
-              <path d="M21 21l-5.2-5.2" />
-            </svg>
-          </a>
+            <a class="btn btn-sm btn-outline-secondary" href="/signup"
+              >Sign up</a
+            >
+          </div>
         </div>
-        <a class="btn btn-sm btn-outline-secondary mx-1" href="/login">Login</a>
-        <a class="btn btn-sm btn-outline-secondary" href="/signup">Sign up</a>
       </div>
     </div>
   </header>
