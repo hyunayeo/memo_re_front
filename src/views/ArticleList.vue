e703a8a6b5ce0665c9797356e9844403f652ba41
captain-yun <jslovejs182@gmail.com>
2023-12-07 18:21:06 +0900

    #44 - 게시글 리스트 조회 기능 구현 - 페이징 기능 추가 - 약간의 css (마우스 hovering 시 effect ) <TODO>  - 게시글 쓰러가기 버튼은 구현했으나 게시글 등록 기능은 부재  - 최신순, 인기순 정렬 기능 추가해야함  - 디자인적인 부분 -> 리스트 크기가 너무 동적이고 데이터 로딩되는거도 너무 동적이라 불편함. 개선 필요.

--- src/views/ArticleList.vue
+++ src/views/ArticleList.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="d-flex justify-content-end align-items-center mb-4">
-    <a href="#" class="text-body-emphasis text-decoration-none">최신순</a>
-    &nbsp;/&nbsp;
-    <a href="#" class="text-body-emphasis text-decoration-none">인기순</a>
+  <div class="d-flex justify-content-end align-items-center">
+    <div class="w-50 mb-4">
+      <button @click="goToWrite" class="btn-link">게시글 쓰러가기</button>
+    </div>
+    <div class="d-flex justify-content-end align-items-center mb-4">
+      <a href="#" class="text-body-emphasis text-decoration-none">최신순</a>
+      &nbsp;/&nbsp;
+      <a href="#" class="text-body-emphasis text-decoration-none">인기순</a>
+    </div>
   </div>
   <p v-if="article">{{article.title}}</p>
   <img v-if="article" :src="article.book.cover">
@@ -19,8 +24,8 @@
       </tr>
     </thead>
     <tbody>
-      <tr @click="goToDetail(article)" v-for="(article, i) in articles" :key="article" class="media position-relative">
-        <th scope="row">{{ i }}</th>
+      <tr @click="goToDetail(article)" v-for="(article) in articles" :key="article" class="media position-relative">
+        <td scope="row">{{ article.id }}</td>
         <td>
           <img
             :src="article.book?.cover"
@@ -36,21 +41,21 @@
           {{article.content }}
         </td>
         <td>{{article.member?.name}}</td>
-        <td>{{article.createdAt}}</td>
+        <td>{{dateShort(`${article.createdAt}`)}}</td>
         <td>{{article.viewCount}}</td>
       </tr>
     </tbody>
   </table>
   <nav aria-label="Page navigation example">
-    <ul class="pagination justify-content-center">
+    <ul class="pagination justify-content-center" ref="pageElem">
       <li class="page-item disabled">
-        <a class="page-link" href="#" tabindex="-1">Previous</a>
+        <a @click="prevPage" class="page-link" ref="previous" tabindex="-1">Previous</a>
       </li>
-      <li @:click="fetchByPage(i)"  v-for="i in pagination.totalPageCount" :key="i" class="page-item">
-        <a class="page-link" href="#">{{i}}</a>
+      <li @:click="fetchByPage(i)"  v-for="i in pages" :key="i" class="page-item">
+        <a class="page-link page-number">{{i}}</a>
       </li>
       <li class="page-item">
-        <a class="page-link" href="#">Next</a>
+        <a @click="nextPage" class="page-link">Next</a>
       </li>
     </ul>
   </nav>
@@ -60,25 +65,41 @@
 import articleApi from '@/api/article.api.js'
 import bookApi from '@/api/book.api';
 import memberApi from '@/api/member.api';
-// import {router} from 'vue';
+import { ref } from 'vue';
 
 export default {
+  setup() {
+    const pageElem = ref(null), previous = ref(null), pageNumber = ref(null);
+    return {
+      pageElem,
+      previous,
+      pageNumber
+    }
+  },
   data() {
     return {
       articles : [],
       pagination : {},
+      searchDto : {
+        page : 1,
+        recordSize : 5,
+        pageSize: 5,
+      },
+      currentEndPage : 5
     }
   },
   async mounted() {
-    this.fetchArticlesWithBookAndMember();
+    this.fetchArticlesWithBookAndMember(this.searchDto);
+  },
+  updated() {
+    this.colorPage()
   },
   methods : {
-    async fetchArticlesWithBookAndMember(params) {
-      let res = await articleApi.getArticles(params);
+    async fetchArticlesWithBookAndMember(searchDto) {
+      let res = await articleApi.getArticles(searchDto);
       this.articles = res.data.list;
       this.pagination = res.data.pagination
-      console.log(this.articles)
-
+      
       this.articles.forEach(async (article) => {
         res = await bookApi.getBook(article.bookId);
         article.book = res.data;
@@ -90,13 +111,67 @@ export default {
       })
     },
     fetchByPage(i) {
-      this.fetchArticlesWithBookAndMember({page : i});
+      this.searchDto.page = i;
+      console.log('page : ' + this.searchDto.page);
+      this.fetchArticlesWithBookAndMember(this.searchDto);
+    },
+    nextPage() {
+      this.fetchByPage(this.pagination.endPage + 1);
+    },
+    prevPage() {
+      this.fetchByPage(this.searchDto.page - 5);
+    },
+    colorPage() {
+
+      let pages = document.querySelectorAll('.page-number');
+      pages.forEach((page) => {
+        if (page.innerHTML == this.searchDto.page) {
+          console.log(page.parentElement)
+          page.classList.add("active");
+        } else {
+          page.classList.remove("active");
+        }
+      })
     },
     goToDetail(article) {
       this.$router.push({ path: `/article/detail/${article.id}` });
+    },
+    goToWrite() {
+      this.$router.push({ path: `/article/insert` });
+    },
+    dateShort(date) {
+      return date.split('T')[0]
+    }
+  },
+  computed : {
+    pages() {
+
+      if (!this.pagination.existNextPage) {
+        this.pageElem?.lastChild.classList.add('disabled')
+      } else {
+        this.pageElem?.lastChild.classList.remove('disabled')
+      }
+      if (!this.pagination.existPrevPage) {
+        this.pageElem?.firstChild.classList.add('disabled')
+      } else {
+        this.pageElem?.firstChild.classList.remove('disabled')
+      }
+
+      let end = this.pagination?.endPage;
+      let start = this.pagination?.startPage;
+
+      let pages = Array.from({length: end}, (_, index) => index + 1);
+
+
+      return pages?.slice(start-1);
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+.pagination > *:hover, tbody>tr:hover {
+  opacity: 0.8;
+  cursor: pointer;
+}
+</style>
