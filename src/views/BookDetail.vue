d93544a117b946ffe0401639c17bc41aba7ed33e
Lyrics109 <rily1108@gmail.com>
2023-12-08 14:09:25 +0900

    가져온 객체 화면에 뿌려주기

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -1,19 +1,63 @@
 <template>
-  <!-- <br />
-  <book-info />
+  <br />
+  <book-info v-bind:book="book" />
+
   <p></p>
   <br />
   <br />
-  <review-card /> -->
-  <h1>{{ book.title }}</h1>
+  <h1>{{ articles?.title }}</h1>
+  <review-card v-bind:articles="articles" />
 </template>
 <script>
+import bookApi from "@/api/book.api";
 import ReviewCard from "@/components/ReviewCard.vue";
 import BookInfo from "@/components/BookInfo.vue";
+import articleApi from "@/api/article.api";
 
 export default {
   name: "BookDetail",
-  components: { ReviewCard, BookInfo },
+
+  components: { BookInfo, ReviewCard },
+
+  data() {
+    return {
+      book: Object,
+      articles: Array,
+    };
+  },
+  async mounted() {
+    console.log("mounted...");
+    this.book = await bookApi
+      .getBook(this.$route.path.split("/")[3])
+
+      .then((response) => {
+        console.log("getBook", response);
+
+        return response.data;
+      })
+      .catch(function (e) {
+        console.log("route 값", this.$route.path);
+        console.log(e);
+      });
+    console.log("proxy가 뒤로", this.book);
+
+    this.articles = await articleApi
+      .getArticles({ searchType: "book_id", searchKeyword: this.book.id })
+      .then((response) => {
+        console.log("getArticles", response);
+        return response.data.list[0];
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+    console.log("뒤로", this.articles);
+  },
+
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 
