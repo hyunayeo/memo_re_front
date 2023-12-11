19e5a29b211d439fe260e2b0ff1f22dbd719abcc
hansolkim <67495641+Lyrics109@users.noreply.github.com>
2023-12-11 13:06:55 +0900

    Feature/#45 book detail 내 컴포넌트 pr (#48)
    * bookdetail
    
    * 가져온 객체 화면에 뿌려주기
    
    * cute insert button
    
    * bookdetail_bookinfo 완, reviewcard 미완

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -1,61 +1,37 @@
 <template>
   <br />
   <book-info v-bind:book="book" />
-
   <p></p>
   <br />
   <br />
-  <h1>{{ articles?.title }}</h1>
-  <review-card v-bind:articles="articles" />
+  <review-card
+    v-for="(article, i) in book.articles"
+    v-bind:article="book.articles[i]"
+    v-bind:key="i"
+  />
 </template>
 <script>
-import bookApi from "@/api/book.api";
 import ReviewCard from "@/components/ReviewCard.vue";
 import BookInfo from "@/components/BookInfo.vue";
-import articleApi from "@/api/article.api";
+import bookApi from "@/api/book.api";
 
 export default {
   name: "BookDetail",
-
   components: { BookInfo, ReviewCard },
-
   data() {
     return {
       book: Object,
-      articles: Array,
+      article: Array,
     };
   },
   async mounted() {
-    console.log("mounted...");
-    this.book = await bookApi
-      .getBook(this.$route.path.split("/")[3])
-
-      .then((response) => {
-        console.log("getBook", response);
-
-        return response.data;
-      })
-      .catch(function (e) {
-        console.log("route 값", this.$route.path);
-        console.log(e);
-      });
-    console.log("proxy가 뒤로", this.book);
-
-    this.articles = await articleApi
-      .getArticles({ searchType: "book_id", searchKeyword: this.book.id })
-      .then((response) => {
-        console.log("getArticles", response);
-        return response.data.list[0];
-      })
-      .catch(function (e) {
-        console.log(e);
-      });
-    console.log("뒤로", this.articles);
+    this.fetchBookById(this.$route.path.split("/")[3]);
   },
-
   methods: {
-    goBack() {
-      this.$router.go(-1);
+    async fetchBookById(id) {
+      let res = await bookApi.getBook(id);
+      console.log("getbook", res);
+      this.book = res.data;
     },
   },
 };
