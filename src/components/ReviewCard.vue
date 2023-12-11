79ae385b3b323131a109307280ff4e74ab831eef
Lyrics109 <rily1108@gmail.com>
2023-12-11 11:47:52 +0900

    bookdetail_bookinfo 완, reviewcard 미완

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -4,8 +4,9 @@
       class="row g-0 border rounded overflow-hidden flex-md-row mb-4 position-relative"
     >
       <div class="col p-4 d-flex flex-column position-static">
-        <h3 class="mb-0">{{ this.articles?.title }}</h3>
-        <!-- <h5>{{ this.members?.name }} (글쓴이)</h5> -->
+        <h3 class="mb-0">{{ this.article.title }}</h3>
+        <h5>{{ this.article.member.name }} (글쓴이)</h5>
+
         <div class="star-rating">
           <span v-for="index in 5" :key="index">
             <span
@@ -16,10 +17,18 @@
             <span v-if="index > articles?.ratingScore">☆</span>
           </span>
         </div>
+        <p class="card-text mb-auto">{{ this.article.content }}</p>
 
-        <p class="card-text mb-auto">{{ this.articles?.content }}</p>
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-          <a class="btn btn-sm btn-outline-secondary" href="#">go to read</a>
+          <!-- <a class="btn btn-sm btn-outline-secondary" href="#"></a> -->
+          <!-- <a
+            @click="goToDetail(article)"
+            v-for="article in articles"
+            :key="article"
+            class="media position-relative"
+          >
+            go to read</a
+          > -->
         </div>
       </div>
     </div>
@@ -29,6 +38,12 @@
 <script>
 export default {
   name: "ReviewCard",
-  props: ["articles"],
+
+  props: ["article"],
+  // methods: {
+  //   goToDetail(article) {
+  //     this.$router.push({ path: `/article/detail/${article.id}` });
+  //   },
+  // },
 };
 </script>
