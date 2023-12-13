edef7e27416f6472680407e542658ca2eaeb715f
devnabi <klkl0812@naver.com>
2023-12-13 15:55:50 +0900

    문의작성한 것 post 요청 추가 (로그인 필요)

--- src/views/AskInsert.vue
+++ src/views/AskInsert.vue
@@ -3,35 +3,48 @@
     <div class="row justify-content-center">
       <h1>문의하기</h1>
 
-      <div class="col-9 form-group">
-        <label class="col-form-label col-form-label-lg mt-4" for="askTitle"
-          >문의 제목</label
-        >
-        <input
-          class="form-control form-control-lg"
-          type="text"
-          maxlength="40"
-          placeholder="글자수 제한 40자"
-          id="askTitle"
-        />
+      <div class="d-grid gap-2 d-md-flex col- justify-content-md-end">
+        <button @click="askInsert" type="submit" class="btn btn-primary btn-lg mx-2">저장</button>
       </div>
 
       <div class="col-9 form-group">
-        <label class="col-form-label col-form-label-lg mt-4" for="askContent"
-          >문의 내용</label
-        >
-        <textarea class="form-control" id="askContent" rows="20"> </textarea>
+        <label class="col-form-label col-form-label-lg mt-4" for="askTitle">문의 제목</label>
+        <input v-model="ask.title" class="form-control form-control-lg" type="text" maxlength="40"
+          placeholder="글자수 제한 40자" id="askTitle" />
       </div>
-    </div>
-    <p></p>
-    <div class="d-grid gap-2 d-md-flex col- justify-content-md-end">
-      <button type="button" class="btn btn-primary btn-lg mx-2">저장</button>
+
+      <div class="col-9 form-group">
+        <label class="col-form-label col-form-label-lg mt-4" for="askContent">문의 내용</label>
+        <textarea v-model="ask.content" class="form-control" id="askContent" rows="20"> </textarea>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      ask: {},
+    }
+  },
+
+  methods: {
+    async askInsert() {
+      console.log("ask", this.ask);
+      await axios.post(`http://localhost:8080/api/questions`, this.ask)
+        .then((response) => {
+          this.ask = response.data;
+          alert("문의 성공! 이메일 답변을 기다려주세요.");
+        })
+        .catch((error) => {
+          console.log("post 오류", error);
+        })
+    },
+  },
+};
 </script>
 
 <style></style>
