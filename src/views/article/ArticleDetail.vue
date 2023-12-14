fc119a17102c0d07758b75819c62b4ffa503c583
kitri.ha <picnic935@gmail.com>
2023-12-14 18:00:08 +0900

    게시글 상세페이지에서 내가 쓴 글일 경우 로직 추가

--- src/views/article/ArticleDetail.vue
+++ src/views/article/ArticleDetail.vue
@@ -1,38 +1,26 @@
 <template>
-  <blog-post :article="article"/>
-  <div class="d-flex justify-content-end align-items-center mb-4">
-    <a class="btn btn-sm btn-outline-secondary mx-1" @click="goToUpdate">update</a>
-    <a class="btn btn-sm btn-outline-secondary" @click="deleteArticle">delete</a>
-  </div>
+  <blog-post :article="article" />
 </template>
 <script>
 import BlogPost from "@/components/article/BlogPost.vue";
-import articleApi from '@/api/article.api';
+import articleApi from "@/api/article.api";
 
 export default {
   components: { BlogPost },
   data() {
     return {
-      article : {}
-    }
+      article: {},
+    };
   },
   async mounted() {
-    this.fetchArticleById(this.$route.path.split('/').pop())
+    this.fetchArticleById(this.$route.path.split("/").pop());
   },
-  methods : {
+  methods: {
     async fetchArticleById(id) {
       let res = await articleApi.getArticle(id);
       this.article = res.data;
     },
-    goToUpdate() {
-      this.$router.push({ path: `/article/update/${this.article.id}` });
-    },
-    deleteArticle() {
-      articleApi.deleteArticle(this.article.id);
-      
-      this.$router.back();
-    }
-  }
+  },
 };
 </script>
 
