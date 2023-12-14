ff15b6b261137717e7bba0e13fb602fc3d09230f
Lyrics109 <rily1108@gmail.com>
2023-12-14 15:47:57 +0900

    DB값 조회해서 wish 상태 표시

--- src/components/book/BookInfo.vue
+++ src/components/book/BookInfo.vue
@@ -19,6 +19,7 @@
             <div class="col p-4 d-flex flex-column position-static">
               <h2 class="display-5 link-body-emphasis mb-1 text-border">
                 {{ this.book?.title }}
+                {{ this.book?.id }}
               </h2>
               <h4>지은이 : {{ this.book?.author }}</h4>
               <h4>출판사 : {{ this.book?.publisher }}</h4>
@@ -30,7 +31,7 @@
           <a
             class="btn btn-sm btn-outline-secondary"
             id="wish"
-            @click="postWish()"
+            @click="addToWishlist()"
             >wish ♡</a
           >
         </div>
@@ -47,26 +48,57 @@
 
 <script>
 import wishApi from "@/api/wish.api";
+import memberApi from "@/api/member.api";
+import { ref } from "vue";
 
 export default {
   name: "BookInfo",
   props: ["book"],
+
+  setup() {
+    const memberId = ref(null);
+
+    const updateMemberId = () => {
+      memberId.value = memberApi.getMemberId();
+      memberId.value = 5; // 이거 빼면 내 로그인 값 나옴
+    };
+    updateMemberId();
+    console.log("멤버아이디", memberId.value);
+    return { memberId };
+  },
   methods: {
-    postWish: function () {
-      const wish = document.getElementById("wish");
+    addToWishlist() {
+      memberApi.checkLogin();
+      this.postWish();
+    },
+    postWish: async function () {
+      const wish = document.querySelector("#wish");
       const hasClass = wish.classList.contains("active");
       if (!hasClass) {
-        // 임시 memberId : 6
-        wishApi.postWish(6, this.book?.id);
+        wishApi.postWish(this.memberId, this.book?.id);
         console.log("잘 들어감...");
         wish.classList.add("active");
       } else {
-        //임시 memberId : 6
-        wishApi.deleteWish(this.book?.id, 6);
+        wishApi.deleteWish(this.book?.id, this.memberId);
         wish.classList.remove("active");
         console.log("잘 지움...");
       }
     },
+    async fetchWishByBookId(bookId) {
+      let res = await wishApi.getWishByBookId(bookId, this.memberId);
+      console.log("methods", res);
+      if (res.data != "") {
+        const wish = document.querySelector("#wish");
+        wish.classList.add("active");
+      } else {
+        const wish = document.querySelector("#wish");
+        wish.classList.remove("active");
+      }
+    },
+  },
+  async updated() {
+    console.log("updated book:", this.book.id);
+    this.fetchWishByBookId(this.book.id);
   },
 };
 </script>
