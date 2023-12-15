73dd747c92ada6fe9f93b7feb9b8aa45f45005cc
devnabi <klkl0812@naver.com>
2023-12-16 08:48:19 +0900

    페이지 넘버 활성화 이벤트, 페이지 초기화, 특정도서데이터 추가 (검색기능 추가해야함),

--- src/views/book/BookList.vue
+++ src/views/book/BookList.vue
@@ -11,6 +11,7 @@
           <th scope="col">book</th>
         </tr>
       </thead>
+
       <tbody>
         <tr>
           <td>
@@ -19,6 +20,7 @@
           </td>
         </tr>
       </tbody>
+
     </table>
 
     <nav aria-label="Page navigation example">
@@ -41,6 +43,9 @@
 import BookMedium from '@/components/book/BookMedium.vue';
 import bookApi from '@/api/book.api';
 
+// const next = document.getElementById("next");
+// const previous = document.getElementById("previous");
+
 export default {
   name: "BookList",
   components: { BookMedium },
@@ -50,6 +55,13 @@ export default {
       bookList: [],
       pagination: {},
 
+      searchDto: {
+        searchKeyword: "",
+        searchType: "title",
+      },
+
+      searchBooks: [],
+      keyword: "",
       currentPage: 1,
       itemsPerPage: 5,
     }
@@ -64,34 +76,67 @@ export default {
       let res = await bookApi.getBooks({ recordSize: 100 });
       this.bookList = res.data.list;
       console.log("getBookList", res);
-      // await axios.get('/api/books')
-      //   .then((response) => {
-      //     console.log("getBookList", response);
-      //     this.bookList = response.data.list;
-      //     return this.bookList;
-      //   })
-      //   .catch((error) => {
-      //     console.error('API 호출 중 오류:', error);
-      //   })
     },
 
+    async searchBookData({ keyword, type }) {
+      console.log("특정 도서 검색");
+      this.searchDto.searchType = type;
+      this.searchDto.searchKeyword = keyword;
+      let res = await bookApi.getBooks(this.searchDto);
+      this.searchBooks = res.data.list;
+      this.pagination = res.data.pagination;
+      return this.searchBooks;
+    },
+
+    // 1. pager가 5개씩 보여야 함 -> 1 ~ 5, 6 ~ 10
+    // 2. 특정 책을 검색했을 때, 그 책 데이터만 보여주기
+    // 3. nextPage() 조건 다시 걸어주기 -> 다음으로 더 넘어가지 않게
     nextPage() {
+      // 최대 페이지가 아닐 경우만 next버튼이 되도록
       if (this.currentPage < this.pageCount) {
+        this.currentPage += 5;
         console.log("다음...");
-        this.currentPage ++;
-        // this.currentPage += 5;
+        console.log("+ 5 = ", this.currentPage);
+        this.goToPage(this.currentPage);
+      }
+      // pageCount보다 작은 수라면 다 되는 중 ...
+      if (this.pageCount >= this.pageCount - 5) {
+        console.log("최대페이지 초기화");
+        this.currentPage = this.pageCount;
+        // next?.classList?.add("disabled");
       }
     },
+
     prevPage() {
+      // 2쪽 이상일 경우만 previous버튼이 되도록
       if (this.currentPage > 1) {
+        this.currentPage -= 5;
         console.log("이전...");
-        this.currentPage --;
-        // this.currentPage -= 5;
+        console.log("- 5 = ", this.currentPage);
+        this.goToPage(this.currentPage);
+      }
+      // 현재페이지가 1로 초기화가 되어야 함
+      if ((this.currentPage <= 5) && (this.currentPage <= 0)) {
+        console.log("1 초기화");
+        this.currentPage = 1;
       }
     },
+
     goToPage(pageNumber) {
+      console.log("스타일 적용");
+      const pageItemAll = document.querySelectorAll(".page-item");
       this.currentPage = pageNumber;
-      // const pageItem = document.querySelector(".page-link");
+
+      // pageNumber 눌렀던 것 비활성화
+      pageItemAll.forEach(item => {
+        item.classList.remove('active', 'disabled');
+      })
+
+      // pageNumber 클릭 시 활성화
+      const pageItem = document.querySelector(`.page-item:nth-child(${pageNumber + 1})`);
+      if (pageItem) {
+        pageItem.classList.add('active');
+      }
     },
   },
 
@@ -105,10 +150,15 @@ export default {
     pageCount() {
       return Math.ceil(this.bookList.length / this.itemsPerPage);
     },
-    // 페이지를 5의 배수까지 나오게
-    showPage() {
-      return Math.ceil(this.pageNumber / this.itemsPerPage);
-    }
+    // // pager 5개씩(ArticleList 복사)
+    // pages() {
+    //   let end = this.pagination?.endPage;
+    //   let start = this.pagination?.startPage;
+
+    //   let pages = Array.from({ length: end }, (_, index) => index + 1);
+
+    //   return pages?.slice(start - 1);
+    // },
   }
 };
 </script>
