aefc76f75315dc3be5b811a1450167979e02d66b
hyunayeo <48472568+hyunayeo@users.noreply.github.com>
2023-12-18 13:42:39 +0900

    도서 검색 페이지 구현 및 검색 시 오류 수정 (#72)
    * 테스트 코드 삭
    
    * 문의하기 수정
    
    * 문의하기 수
    
    * 검색오류 수
    
    * 책리스트 이미지 추가 및 위시 버튼 상단으로 이동
    
    * 책목록 조회 구현 중
    
    * 도서 검색 및 마이페이지 찜 도서 구현
    
    * 도서 리스트에서 도서상세 페이지 넘어갈 때 오류 수정

--- src/views/book/BookList.vue
+++ src/views/book/BookList.vue
@@ -15,59 +15,153 @@
         <tr>
           <td>
             <!-- @click="goToDetail($event.target, book.id)" -->
-            <card-medium v-for="(book, i) in bookList" :key="i" v-bind:book="book"/>
+            <BookMedium
+              v-for="(book, i) in bookList"
+              :key="i"
+              v-bind:book="book"
+            />
           </td>
         </tr>
       </tbody>
     </table>
     <nav aria-label="Page navigation example">
-      <ul class="pagination justify-content-center">
+      <ul class="pagination justify-content-center" ref="pageElem">
         <li class="page-item disabled">
-          <a class="page-link" href="#" tabindex="-1">Previous</a>
+          <a @click="prevPage" class="page-link" ref="previous" tabindex="-1"
+            >Previous</a
+          >
         </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#">Next</a>
+        <li
+          @:click="fetchByPage(i)"
+          v-for="i in pages"
+          :key="i"
+          class="page-item"
+        >
+          <a
+            :class="[
+              'page-link page-number',
+              { active: this.searchDto.page == i },
+            ]"
+            >{{ i }}</a
+          >
+        </li>
+        <li class="page-item disabled">
+          <a @click="nextPage" class="page-link" ref="next" tabindex="-1"
+            >next</a
+          >
         </li>
       </ul>
     </nav>
   </div>
 </template>
 <script>
-import CardMedium from "@/components/book/BookMedium.vue";
-// import bookApi from "@/api/book.api";
-import axios from 'axios';
+import BookMedium from "@/components/book/BookMedium.vue";
+import { ref } from "vue";
+import bookApi from "@/api/book.api";
+import wishApi from "@/api/wish.api";
+import memberApi from "@/api/member.api";
 
 export default {
   name: "BookList",
-  components: { CardMedium },
+  components: { BookMedium },
+  setup() {
+    const pageElem = ref(null),
+      previous = ref(null),
+      pageNumber = ref(null);
+    return {
+      pageElem,
+      previous,
+      pageNumber,
+    };
+  },
 
   data() {
     return {
       bookList: [],
-    }
+      pagination: {},
+      searchDto: {
+        page: 1,
+        recordSize: 5,
+        pageSize: 5,
+        searchType: "",
+        searchKeyword: "",
+      },
+      currentEndPage: 5,
+      isFromMyPage: false,
+    };
   },
 
   mounted() {
+    this.pathCheck();
     this.fetchBookData();
   },
 
   methods: {
+    pathCheck() {
+      if (this.$router.options.history.state.back) {
+        this.isFromMyPage = true;
+        this.searchDto.searchType = "member_id";
+        this.searchDto.searchKeyword = memberApi.getMemberId();
+      }
+      if (this.$route.query?.type == "도서명") {
+        this.searchDto.searchType = "title";
+        this.searchDto.searchKeyword = this.$route.query.keyword;
+      } else if (this.$route.query?.type == "작가명") {
+        this.searchDto.searchType = "author";
+        this.searchDto.searchKeyword = this.$route.query.keyword;
+      }
+    },
     async fetchBookData() {
-      await axios.get('/api/books')
-        .then((response) => {
-          console.log("getBookList", response);
-          this.bookList = response.data.list;
-          return this.bookList;
-        })
-        .catch((error) => {
-          console.error('API 호출 중 오류:', error);
-        })
+      let res;
+      if (this.isFromMyPage == true) {
+        let res = await wishApi.getWishes(this.searchDto);
+        this.bookList = res.data.list.map((data) => data.book);
+        this.pagination = res.data.pagination;
+      } else {
+        res = await bookApi.getBooks(this.searchDto);
+        this.bookList = res.data.list;
+        console.log(this.bookList);
+        this.pagination = res.data.pagination;
+      }
+    },
+    fetchByPage(i) {
+      this.searchDto.page = i;
+      this.fetchBookData(this.searchDto);
+    },
+    nextPage() {
+      this.fetchByPage(this.pagination.endPage + 1);
+    },
+    prevPage() {
+      this.fetchByPage(this.searchDto.page - 5);
+    },
+  },
+
+  computed: {
+    pages() {
+      if (!this.pagination?.existNextPage) {
+        this.pageElem?.lastChild.classList?.add("disabled");
+      } else {
+        this.pageElem?.lastChild.classList?.remove("disabled");
+      }
+      if (!this.pagination?.existPrevPage) {
+        this.pageElem?.firstChild.classList?.add("disabled");
+      } else {
+        this.pageElem?.firstChild.classList?.remove("disabled");
+      }
+
+      let end = this.pagination?.endPage;
+      let start = this.pagination?.startPage;
+      let pages = Array.from({ length: end }, (_, index) => index + 1);
+      return pages?.slice(start - 1);
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.pagination > *:hover,
+tbody > tr:hover {
+  opacity: 0.8;
+  cursor: pointer;
+}
+</style>
