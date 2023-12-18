677a1be560b59152679a7f7ec9332f0916b9d957
devnabi <klkl0812@naver.com>
2023-12-18 11:19:20 +0900

    특정도서요청 추가, 페이지넘버 5개씩 보이는 기능 추가

--- src/views/book/BookList.vue
+++ src/views/book/BookList.vue
@@ -15,7 +15,6 @@
       <tbody>
         <tr>
           <td>
-            <!-- @click="goToDetail($event.target, book.id)" -->
             <book-medium v-for="(book, i) in books" :key="i" v-bind:book="book" />
           </td>
         </tr>
@@ -28,8 +27,8 @@
         <li class="page-item">
           <a @click="prevPage" class="page-link" id="previous" tabindex="-1">Previous</a>
         </li>
-        <li class="page-item" v-for="pageNumber in pageCount" :key="pageNumber">
-          <a @click="goToPage(pageNumber)" class="page-link">{{ pageNumber }}</a>
+        <li class="page-item" v-for="pageNumber in pages" :key="pageNumber">
+          <a @click="goToPage(pageNumber)" :class="{ active: pageNumber === currentPage }" class="page-link">{{ pageNumber }}</a>
         </li>
         <li class="page-item">
           <a @click="nextPage" class="page-link" id="next">Next</a>
@@ -42,9 +41,7 @@
 <script>
 import BookMedium from '@/components/book/BookMedium.vue';
 import bookApi from '@/api/book.api';
-
-// const next = document.getElementById("next");
-// const previous = document.getElementById("previous");
+import { useRoute } from 'vue-router';
 
 export default {
   name: "BookList",
@@ -54,43 +51,60 @@ export default {
     return {
       bookList: [],
       pagination: {},
-
       searchDto: {
+        searchType: "",
         searchKeyword: "",
-        searchType: "title",
+        recordSize: 100,
       },
 
-      searchBooks: [],
-      keyword: "",
       currentPage: 1,
       itemsPerPage: 5,
     }
   },
 
   mounted() {
+    // - [ ] 검색을 할 때에만 (어떻게 체크할지 생각해보기) searchDto에 해당 파라미터들을 넘겨주기
+    // - [ ] fetchBookData
     this.fetchBookData();
+    const route = useRoute();
+    console.log(route);
+    // const keyword = route.query.keyword;
+    // const type = route.query.type;
+    // this.searchBookData({ keyword, type });
+    // this.pathCheck();
   },
 
   methods: {
+    // 전체 도서 조회
     async fetchBookData() {
       let res = await bookApi.getBooks({ recordSize: 100 });
       this.bookList = res.data.list;
       console.log("getBookList", res);
     },
 
-    async searchBookData({ keyword, type }) {
-      console.log("특정 도서 검색");
-      this.searchDto.searchType = type;
-      this.searchDto.searchKeyword = keyword;
-      let res = await bookApi.getBooks(this.searchDto);
-      this.searchBooks = res.data.list;
-      this.pagination = res.data.pagination;
-      return this.searchBooks;
+    // async searchBookData({ keyword, type }) {
+    //   console.log("특정 도서 검색", keyword, type);
+    //   this.searchDto.searchType = type;
+    //   this.searchDto.searchKeyword = keyword;
+    //   let res = await bookApi.getBooks(this.searchDto);
+    //   console.log(res);
+    //   this.bookList = res.data.list;
+    //   this.pagination = res.data.pagination;
+    //   return this.bookList;
+    // },
+
+    pathCheck() {
+      // const searchType = (this.type == "도서명") ? "title" : "author";
+      if (this.$route.query?.type == "도서명") {
+        this.searchDto.searchType = "title";
+        this.searchDto.searchKeyword = this.$route.query.keyword;
+      } else if (this.$route.query?.type == "작가명") {
+        this.searchDto.searchType = "author";
+        this.searchDto.searchKeyword = this.$route.query.keyword;
+      }
     },
 
     // 1. pager가 5개씩 보여야 함 -> 1 ~ 5, 6 ~ 10
-    // 2. 특정 책을 검색했을 때, 그 책 데이터만 보여주기
-    // 3. nextPage() 조건 다시 걸어주기 -> 다음으로 더 넘어가지 않게
     nextPage() {
       // 최대 페이지가 아닐 경우만 next버튼이 되도록
       if (this.currentPage < this.pageCount) {
@@ -99,11 +113,9 @@ export default {
         console.log("+ 5 = ", this.currentPage);
         this.goToPage(this.currentPage);
       }
-      // pageCount보다 작은 수라면 다 되는 중 ...
-      if (this.pageCount >= this.pageCount - 5) {
+      if (this.currentPage > this.pageCount) {
         console.log("최대페이지 초기화");
         this.currentPage = this.pageCount;
-        // next?.classList?.add("disabled");
       }
     },
 
@@ -150,15 +162,11 @@ export default {
     pageCount() {
       return Math.ceil(this.bookList.length / this.itemsPerPage);
     },
-    // // pager 5개씩(ArticleList 복사)
-    // pages() {
-    //   let end = this.pagination?.endPage;
-    //   let start = this.pagination?.startPage;
-
-    //   let pages = Array.from({ length: end }, (_, index) => index + 1);
-
-    //   return pages?.slice(start - 1);
-    // },
+    pages() {
+      const start = Math.floor((this.currentPage - 1) / 5) * 5 + 1;
+      const end = Math.min(start + 4, this.pageCount);
+      return Array.from({ length: end - start + 1 }, (_, index) => start + index);
+    },
   }
 };
 </script>
