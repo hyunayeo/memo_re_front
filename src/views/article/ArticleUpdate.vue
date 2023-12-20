98c00612e4c77015b31342eb08547680cf13716f
Lyrics109 <rily1108@gmail.com>
2023-12-20 13:17:34 +0900

    여백 줄이기 및 wish 카드 오류 해결

--- src/views/article/ArticleUpdate.vue
+++ src/views/article/ArticleUpdate.vue
@@ -1,128 +1,130 @@
 <template>
-  <div class="mt-5 pt-5">
-    <div class="container-fluid px-4">
-      <h1 class="mt-4">Board</h1>
-      <div class="card mb-4">
-        <div class="card-body">
-          <form method="post">
-            <label for="bookInfo" class="form-label">책 정보</label>
-            <div
-              class="d-grid gap-2 d-md-flex justify-content-md-start"
-              @click="showModal = true"
-            >
-              <a
-                href="#"
-                class="btn btn-primary"
-                role="button"
-                data-bs-toggle="button"
-                >책 정보 수정
-              </a>
-            </div>
-            <!-- <card-small-vue :article="article"/> -->
-            <BookSearch
-              v-if="showModal"
-              @close="showModal = false"
-              @showRegisterBook="(show) => (showRegisterModal = show)"
-              @picked="pickBook"
-            />
-            <BookRegistration
-              v-if="showRegisterModal"
-              @close="showRegisterModal = false"
-            />
-
-            <book-small-vue :book="article.book" />
-
-            <div class="mb-3 mt-3">
-              <label for="title" class="form-label">제목</label>
-              <input
-                type="text"
-                class="form-control"
-                id="title"
-                name="title"
-                placeholder="제목을 입력해 주세요."
-                v-model="article.title"
+  <div class="container px-4">
+    <h1 class="mt-4">수정하기</h1>
+    <div class="b-row">
+      <div class="col-10 mx-auto">
+        <div class="card mb-4">
+          <div class="card-body">
+            <form method="post">
+              <label for="bookInfo" class="form-label">책 정보</label>
+              <div
+                class="d-grid gap-2 d-md-flex justify-content-md-start"
+                @click="showModal = true"
+              >
+                <a
+                  href="#"
+                  class="btn btn-primary"
+                  role="button"
+                  data-bs-toggle="button"
+                  >책 정보 수정
+                </a>
+              </div>
+              <!-- <card-small-vue :article="article"/> -->
+              <BookSearch
+                v-if="showModal"
+                @close="showModal = false"
+                @showRegisterBook="(show) => (showRegisterModal = show)"
+                @picked="pickBook"
               />
-            </div>
+              <BookRegistration
+                v-if="showRegisterModal"
+                @close="showRegisterModal = false"
+              />
 
-            <label for="title" class="form-label">평점</label>
-            <br />
+              <book-small-vue :book="article.book" />
 
-            <div class="star-rating d-flex">
-              <span v-for="index in 5" :key="index" @click="check(index)">
-                <span class="text-warning" v-if="ratingCheck(index)">★</span>
-                <span v-else>☆</span>
-              </span>
-            </div>
-
-            <p></p>
-
-            <form class="row g-6">
-              <div class="col-md-4">
-                <label for="startDate" class="form-label">시작일</label>
+              <div class="mb-3 mt-3">
+                <label for="title" class="form-label">제목</label>
                 <input
-                  type="date"
+                  type="text"
                   class="form-control"
-                  name="startDate"
-                  id="startDate"
-                  v-model="article.startDate"
+                  id="title"
+                  name="title"
+                  placeholder="제목을 입력해 주세요."
+                  v-model="article.title"
                 />
               </div>
+
+              <label for="title" class="form-label">평점</label>
+              <br />
+
+              <div class="star-rating d-flex">
+                <span v-for="index in 5" :key="index" @click="check(index)">
+                  <span class="text-warning" v-if="ratingCheck(index)">★</span>
+                  <span v-else>☆</span>
+                </span>
+              </div>
+
               <p></p>
-              <div class="col-md-4">
-                <label for="endDate" class="form-label">종료일</label>
-                <input
-                  type="date"
+
+              <form class="row g-6">
+                <div class="col-md-4">
+                  <label for="startDate" class="form-label">시작일</label>
+                  <input
+                    type="date"
+                    class="form-control"
+                    name="startDate"
+                    id="startDate"
+                    v-model="article.startDate"
+                  />
+                </div>
+                <p></p>
+                <div class="col-md-4">
+                  <label for="endDate" class="form-label">종료일</label>
+                  <input
+                    type="date"
+                    class="form-control"
+                    name="endDate"
+                    id="endDate"
+                    v-model="article.endDate"
+                  />
+                </div>
+              </form>
+
+              <p></p>
+
+              <div class="mb-3">
+                <label for="content" class="form-label">내용</label>
+                <textarea
                   class="form-control"
-                  name="endDate"
-                  id="endDate"
-                  v-model="article.endDate"
+                  id="content"
+                  name="content"
+                  rows="3"
+                  v-model="article.content"
+                ></textarea>
+              </div>
+
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="isDone"
+                  v-model="article.isDone"
                 />
+                <label class="form-check-label" for="isDone">
+                  다 읽었어요!
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="isHide"
+                  v-model="article.isHide"
+                />
+                <label class="form-check-label" for="isHide"> 비밀글 </label>
+              </div>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button
+                  @click="updateArticle()"
+                  class="btn btn-primary"
+                  type="button"
+                >
+                  수정하기
+                </button>
               </div>
             </form>
-
-            <p></p>
-
-            <div class="mb-3">
-              <label for="content" class="form-label">내용</label>
-              <textarea
-                class="form-control"
-                id="content"
-                name="content"
-                rows="3"
-                v-model="article.content"
-              ></textarea>
-            </div>
-
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                id="isDone"
-                v-model="article.isDone"
-              />
-              <label class="form-check-label" for="isDone">
-                다 읽었어요!
-              </label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                id="isHide"
-                v-model="article.isHide"
-              />
-              <label class="form-check-label" for="isHide"> 비밀글 </label>
-            </div>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-              <button
-                @click="updateArticle()"
-                class="btn btn-primary"
-                type="button"
-              >
-                수정하기
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
